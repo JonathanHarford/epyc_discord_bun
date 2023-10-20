bc424b0887cf057244cd5d75ff6ca5de9bbb5a6e
jonathanharford <jonathan.harford@gmail.com>
2023-10-20 14:22:58 -0700

    Update help image

--- src/copy.ts
+++ src/copy.ts
@@ -41,7 +41,7 @@ export const render = (message: Message): MessageRender => {
                 "draws a picture of that sentence, the third player writes a sentence of that " +
                 "picture, and so on. At the end of the game, it gets posted to #epyc where players " +
                 "can admire the art, the writing, and how the concept mutated. Type `/play` to play.",
-            imageUrl: 'https://i.imgur.com/ipoiOMF.jpeg',
+            imageUrl: 'https://imgur.com/Jmf0SBd.jpg',
         };
         case "status": return {
             title: "current status",
